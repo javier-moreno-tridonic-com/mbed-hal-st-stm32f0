3a14e54fdc587a81023f6b37a99187b976e1691b
Markus Becker <markus.becker@tridonic.com>
2016-06-30 14:15:28 +0200

    Add drivers for the internal flash. Merge conflict solved.

--- source/internal_flash_api.c
+++ source/internal_flash_api.c
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-/*
- * internal_flash_api.h
- *
- *  Created on: 11. Apr. 2016
- *      Author: martin.tomasini2
- */
-#include "internal_flash_api.h"
-#include "cmsis.h"
-
-int WriteFlash(uint32_t ui32Address, const uint8_t* pData, uint32_t ui32DataSize)
-{
-    volatile uint16_t uintDataBuffer;
-
-    HAL_FLASH_Unlock();
-
-    // Write payload byte per byte
-    for(uint32_t i = 0; i < ui32DataSize; i += 2, ui32Address += 2)
-    {
-        if(i < ui32DataSize - 1)
-        {
-            uintDataBuffer = pData[i];
-            uintDataBuffer +=  (uint16_t)(pData[i + 1] << 8);
-        }
-        else
-        {
-            uintDataBuffer = pData[i];
-            uintDataBuffer += 0xFF00;
-        }
-        if(HAL_OK !=  HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD, ui32Address, (uint64_t)uintDataBuffer))
-        {
-            return -1;
-        }
-    }
-
-    HAL_FLASH_Lock();
-
-    return 0;
-
-}
-
-int ReadFlash(uint32_t pui32Address, uint8_t* pData, uint32_t ui32DataSize)
-{
-
-}
-
-int EraseSector(uint32_t ui32Sector)
-{
-
-}
-
-int GetNumOfSectors(uint32_t* pui32NumOfSectors)
-{
-
-}
-
-int GetSectorStartAdd(uint32_t ui32Sector, uint32_t* pui32Address)
-{
-
-}
-
-int GetSectorEndAdd(uint32_t ui32Sector, uint32_t* pui32Address)
-{
-
-}
-
-
-=======
 /*
  * internal_flash_api.h
  *
@@ -204,4 +136,3 @@ void GetSectorEndAdd(uint32_t ui32Sector, uint32_t* pui32Address)
 }
 
 
->>>>>>> 995a20f... Add drivers for the internal flash.
